92f3f2c20702ceea87631736842a8c6051bdc6e5
libbyou <lcyou95@gmail.com>
2023-11-07 18:48:38 -0600

    models made

---
+++ models/index.js
@@ -0,0 +1,32 @@
+const User = require('./user');
+const Posts = require('./posts');
+const Comments = require('./comments');
+
+User.hasMany(Posts, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+  });
+
+  Posts.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+
+  User.hasMany(Comments, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+  });
+
+  Comments.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+
+  Posts.hasMany(Comments, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+  });
+
+  Comments.belongsTo(Posts, {
+    foreignKey: 'post_id'
+  });
+
+module.exports = { User, Posts, Comments };
